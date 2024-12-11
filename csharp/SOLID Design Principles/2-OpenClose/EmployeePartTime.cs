200cc5f3fd5159c6f8a9022de5a106a9c90cbc82
TC-rGalvan <rgalvan@tigerconnect.com>
2024-12-11 12:25:28 -0600

    move salary calculation to its corresponding type

--- csharp/SOLID Design Principles/2-OpenClose/EmployeePartTime.cs
+++ csharp/SOLID Design Principles/2-OpenClose/EmployeePartTime.cs
@@ -1,14 +1,26 @@
 namespace OpenClose
 {
-    public class EmployeePartTime
+    public class EmployeePartTime : Employee
     {
-        public string Fullname { get; set; }
-        public int HoursWorked { get; set; }
-
         public EmployeePartTime(string fullname, int hoursWorked)
         {
             Fullname = fullname;
             HoursWorked = hoursWorked;
-        }        
+            HourValue = 20000M;
+        }
+        
+        public override void CalculateSalaryMonthly()
+        {
+            decimal salary = HourValue * HoursWorked;
+
+            if (HoursWorked > 160) 
+            {
+                decimal effortCompensation = 5000M;
+                int extraDays = HoursWorked - 160;
+                salary += effortCompensation * extraDays;
+            }
+
+            Console.WriteLine($"PartTime - Empleado: {Fullname}, Pago: {salary:C1} ");
+        }
     }
 }
